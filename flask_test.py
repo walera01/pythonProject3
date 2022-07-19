55a5ae565914e5088e26a219bfbae75e3ac919d3
walera01 <wladislaw1004@gmail.com>
2022-07-19 16:54:57 +0300

    Flask day1

--- flask_test.py
+++ flask_test.py
@@ -2,49 +2,44 @@ from flask import Flask, render_template, request, flash
 import pymysql
 from connector import host,password,user,db_name
 
-
-try:
-    connection = pymysql.connect(
-        host = host,
-        password = password,
-        port=3306,
-        user=user,
-        database=db_name,
-        cursorclass = pymysql.cursors.DictCursor,
-    )
-    print("successfully ")
+def con_sql():
     try:
-        connect  = connection.cursor()
-        # create_table = "CREATE TABLE `dildon`(id int AUTO_INCREMENT,"\
-        #         "name varchar(35),"\
-        #         "cost varchar (8),"\
-        #         "PRIMARY KEY(id));"
-        # connect.execute(create_table)
-        weight = '80sm'
+        connection = pymysql.connect(
+            host = host,
+            password = password,
+            port=3306,
+            user=user,
+            database=db_name,
+            cursorclass = pymysql.cursors.DictCursor,
+        )
+        print("Connect")
+        connect = connection.cursor()
+        return connection
+    except Exception as ex:
+        return "Dont connect"
 
-        input_data = "INSERT INTO `dildon`(name, cost) VALUES (%s,'20$'),('25sm','25$');"
-        connect.execute(input_data, weight)
-        connection.commit()
-
-        output_data = "SELECT * FROM `dildon` ORDER BY id DESC ;"
-        connect.execute(output_data)
-        out = connect.fetchall()
-        print("-"*40)
-
-        for i in out:
-            print(i['id'],i['name'], '   =  ', i['cost'])
-            print("-"*40)
-
-        # delete = "DROP TABLE `dildon`"
-        # connect.execute(delete)
-    finally:
-        connection.close()
-except Exception as ex:
-    print("Dont connect")
-    print(ex)
+def close_conect(connection):
+    connection.close()
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='sdfasdfasd'
+
+@app.route('/add')
+def add_db():
+    connection = con_sql()
+    connect = connection.cursor()
+    try:
+        create_table = "CREATE TABLE IF NOT EXISTS `product`(id int AUTO_INCREMENT,"\
+                "name varchar(255)," \
+                "description varchar(35000)," \
+                "cost varchar (8)," \
+                "name varchar(35)," \
+                "PRIMARY KEY(id));"
+        connect.execute(create_table)
+    except:
+        print("Error", Exception)
+
+    return render_template('add.html', title="Добавление записи")
 
 @app.route('/')
 def main():
@@ -64,5 +59,33 @@ def login():
 def pageNotFound(error):
     return render_template('page404.html', title="Ошибка")
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+    # try:
+    # create_table = "CREATE TABLE `dildon`(id int AUTO_INCREMENT,"\
+    #         "name varchar(35),"\
+    #         "cost varchar (8),"\
+    #         "PRIMARY KEY(id));"
+    # connect.execute(create_table)
+    # weight = '80sm'
+    #
+    # input_data = "INSERT INTO `dildon`(name, cost) VALUES (%s,'20$'),('25sm','25$');"
+    # connect.execute(input_data, weight)
+    # connection.commit()
+    #
+    # output_data = "SELECT * FROM `dildon` ORDER BY id DESC ;"
+    # connect.execute(output_data)
+    # out = connect.fetchall()
+    # print("-"*40)
+    #
+    # for i in out:
+    #     print(i['id'],i['name'], '   =  ', i['cost'])
+    #     print("-"*40)
+    #
+    # # delete = "DROP TABLE `dildon`"
+    # # connect.execute(delete)
+
+
